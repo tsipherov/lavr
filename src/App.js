56ad2c2d4b591c621c7866c19df2f0d802834272
Tsiferov <tsiferov@ukr.net>
2021-02-02 19:33:08 +0200

    +onBlur event input

--- src/App.js
+++ src/App.js
@@ -1,75 +1,82 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends React.Component {
-
   static propTypes = {
     min: PropTypes.number.isRequired,
-    max: PropTypes.number.isRequired
-  }
+    max: PropTypes.number.isRequired,
+  };
 
   state = {
-    cnt: this.props.min
+    cnt: this.props.min,
+    inputValue: this.props.min,
   };
 
   increase = () => {
     this.set(this.state.cnt + 1);
-  }
+  };
 
   decrease = () => {
     this.set(this.state.cnt - 1);
-  }
+  };
 
   set(newCnt) {
     // let cnt = Math.min(Math.max(newCnt, this.props.min), this.props.max);
     let cnt = Math.max(Math.min(newCnt, this.props.max), this.props.min);
-    this.setState({ cnt });
+    this.setState({
+      cnt,
+      inputValue: cnt,
+    });
   }
 
-  setValue(newStr){
-    let cnt = parseInt(newStr);
-    this.set(isNaN(cnt) ? this.props.min : cnt);
+  setValue(newStr) {
+    this.setState({ inputValue: newStr });
   }
-  
+
+  applyValue = () => {
+    let cnt = parseInt(this.state.inputValue);
+    this.set(isNaN(cnt) ? this.props.min : cnt);
+  };
+
+  checkEnterKey = (e) => {
+    if (e.keyCode === 13) {
+      this.applyValue();
+    }
+  };
+
   styles = {
     display: "flex",
     alignItems: "center",
     width: 150,
     justifyContent: "space-around",
-    fontSize: 30
-  }
+    fontSize: 30,
+  };
 
   render() {
-
     return (
-      <div className="App" >
+      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <br />
           <div style={this.styles}>
-            <button
-              className="App-link"
-              onClick={this.decrease}
-            >
+            <button className="App-link" onClick={this.decrease}>
               -1
             </button>
-            <p>
-              {this.state.cnt}
-            </p>
-            <button
-              className="App-link"
-              onClick={this.increase}
-            >
+            <p>{this.state.cnt}</p>
+            <button className="App-link" onClick={this.increase}>
               +1
-        </button>
-            </div>
-            <br />
-            <input value={this.state.cnt} 
-                       onChange={(e) => this.setValue(e.target.value)} 
-            />
+            </button>
+          </div>
+          <br />
+          <input
+            value={this.state.inputValue}
+            onChange={(e) => this.setValue(e.target.value)}
+            onBlur={this.applyValue}
+            onKeyUp={this.checkEnterKey}
+          />
         </header>
       </div>
     );
